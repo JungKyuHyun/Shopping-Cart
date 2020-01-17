77da36ea92fa697e0026093162d0514b561e7e5e
Master Jung <ajdkfl6445@gmail.com>
2020-01-17 22:29:41 +0900

    Initialize store-project
    1. 필요한 종속성 설치 및 설정 완료 - 나머지 부분은 필요시마다 설치
    2. 웹팩 dev-server 설치, 설정 및 테스트 완료
    3. eslint, prettier, babel 설정 완료

---
+++ src/index.tsx
@@ -0,0 +1,6 @@
+import React from "react";
+import { render } from "react-dom";
+
+const App = () => <div>Test</div>;
+
+render(<App />, document.getElementById("root"));
---
+++ webpack.config.js
@@ -0,0 +1,78 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const webpack = require('webpack');
+const { CleanWebpackPlugin } = require('clean-webpack-plugin');
+const TsConfigPathsPlugin = require('tsconfig-paths-webpack-plugin');
+
+module.exports = {
+  entry: './src/index.tsx',
+  mode: process.env.NODE_ENV === 'production' ? 'production' : 'development',
+
+  // Enable sourcemaps for debugging webpack's output.
+  devtool: 'source-map',
+
+  resolve: {
+    // Add '.ts' and '.tsx' as resolvable extensions.
+    extensions: ['.ts', '.tsx', '.js'],
+    plugins: [new TsConfigPathsPlugin()],
+  },
+
+  output: {
+    filename: '[name].js',
+    path: path.resolve(__dirname, 'dist'),
+    publicPath: '/',
+  },
+
+  module: {
+    rules: [
+      {
+        test: /\.ts(x?)$/,
+        exclude: /node_modules/,
+        use: [
+          {
+            loader: 'ts-loader',
+          },
+        ],
+      },
+      {
+        test: /\.css$/,
+        use: ['style-loader', 'css-loader'],
+      },
+      // All output '.js' files will have any sourcemaps re-processed by 'source-map-loader'.
+      {
+        enforce: 'pre',
+        test: /\.js$/,
+        loader: 'source-map-loader',
+      },
+    ],
+  },
+
+  devServer: {
+    contentBase: path.join(__dirname, 'dist'),
+    compress: true,
+    historyApiFallback: true,
+    hot: true,
+    port: 3001,
+    host: 'localhost',
+    open: true,
+  },
+
+  plugins: [
+    new webpack.NoEmitOnErrorsPlugin(),
+    new webpack.HotModuleReplacementPlugin(),
+    new webpack.ProgressPlugin(),
+    new CleanWebpackPlugin(),
+    new HtmlWebpackPlugin({
+      template: './src/index.html',
+    }),
+  ],
+
+  // When importing a module whose path matches one of the following, just
+  // assume a corresponding global variable exists and use that instead.
+  // This is important because it allows us to avoid bundling all of our
+  // dependencies, which allows browsers to cache those libraries between builds.
+  // externals: { // NOTE: 원래 이걸 설정하라고 하지만, 오히려 설정하면 에러가 난다.
+  //     "react": "React",
+  //     "react-dom": "ReactDOM"
+  // }
+};
